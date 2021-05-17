b969b1066df0a6944ef549f6dc5071b8c20b5c5b
mehulmakavana <mehulmakavana52@gmail.com>
2021-05-17 16:21:47 +0530

    ok

--- Restaurant-main/Restaurant-main/src/Pages/Dashboard.js
+++ Restaurant-main/Restaurant-main/src/Pages/Dashboard.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import '../Pages/Dashboard.css'
+import './ManagerHome.css';
+import { Link } from 'react-router-dom';
+import Sidesection from './Sidesection';
 import { HiShoppingCart } from 'react-icons/hi';
 import { FaStar } from 'react-icons/fa';
 import { FaClipboardList } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
 
 export class Dashboard extends Component {
 
@@ -32,16 +33,11 @@ export class Dashboard extends Component {
     return (
 
       <div>
+        <div className="flex-category1">
 
-        <h1>
-          DashBoard
-          </h1>
+          <div className="flex-category2">
 
-        <div className="flex1">
-
-          <div className="flex2">
-
-          <Link className="link" to="/Category">
+          <Link className="link" to="/category">
             <h2>TOTAL CATEGORY
               <div>{this.state.category}</div>
             </h2>
@@ -80,10 +76,10 @@ export class Dashboard1 extends Component {
   render() {
     return (
       <div>
-        <div className="flex1">
-          <div className="flex2">
+        <div className="flex-category1">
+          <div className="flex-category2">
 
-            <Link className="link" to="/OrdersHistory">
+            <Link className="link" to="/all-order" >
             <h2>TOTAL ORDER
               <div>{this.state.order}</div>
             </h2>
@@ -107,6 +103,50 @@ export class Dashboard2 extends Component {
     super(props);
     this.state = {
 
+      complaints: "",
+
+    }
+  }
+
+  async componentDidMount() {
+
+    const url = "http://localhost:8020/complaint/complaints"
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ complaints: data.totalItems });
+    this.searchArray = data
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="flex-category1">
+          <div className="flex-category2">
+
+            <Link className="link" to="/complaint" >
+            <h2>TOTAL COMPLAINT
+              <div>{this.state.complaints}</div>
+            </h2>
+            
+
+
+            <h3> <FaClipboardList style={{ fontSize: 40 }} /> </h3>
+            </Link>
+          </div>
+        </div>
+         
+      </div>         
+    )
+  }
+}
+
+export class Dashboard3 extends Component {
+
+  constructor(props) {
+
+    super(props);
+    this.state = {
+
       rating:""
 
     }
@@ -124,17 +164,24 @@ export class Dashboard2 extends Component {
   render() {
 
     return (
-
       <div>
+         <Sidesection />
+
+<h1 className='title-managerpage'>
+  Dashboard
+  </h1>
+      
+
+      <div className="DashView-Manager">
 
         <Dashboard />
         <Dashboard1 />
-      
+        <Dashboard2 />
 
-        <div className="flex3">
-          <div className="flex4">
+        <div className="flex-category3">
+          <div className="flex-category4">
 
-          <Link className="link" to="/Ratings">
+          <Link className="link" to="/review">
             <h2>AVERAGE RATTING
               <div>{this.state.rating}</div>
             </h2>
@@ -143,11 +190,12 @@ export class Dashboard2 extends Component {
             </Link>
           </div>
         </div>
-        </div>
 
+      </div>
+      </div>
     )
   }
 
 }
 
-export default Dashboard2;
+export default Dashboard3;
